7e8af25d7e8a313894bc4ed3ba2173883447404f
BS32 <eguzmandls@gmail.com>
2023-11-06 02:32:17 -0500

    we need more components

--- src/navigation/navbar.js
+++ src/navigation/navbar.js
@@ -1,34 +1,31 @@
 import { Component } from "react";
 import {
-  Nav,
-  NavBtn,
-  NavBtnLink,
-  NavMenu,
-  Bars,
-  NavLink,
+    Nav,
+    NavBtn,
+    NavBtnLink,
+    NavMenu,
+    Bars,
+    NavLink,
 } from "./navcomponents";
 
 class Navbar extends Component {
-  render() {
-    return (
-      <>
-        <Nav>
-          <Bars />
-
-                <NavMenu>
-                    <NavLink to = "/suggestion">Suggestion</NavLink>                    
-                </NavMenu>
-                <NavMenu>
-                    <NavLink to = "/personalgoals">Personal Goals</NavLink>                    
-                </NavMenu>
-                <NavBtn>
-                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
-                </NavBtn>
-            </Nav>
-        </>
-    )
+    render() {
+        return (
+            <>
+                <Nav>
+                    <Bars />
+                    <NavMenu>
+                        <NavLink to="/suggestion" activeStyle>Suggestion</NavLink>
+                        <NavLink to="/personalgoals" activeStyle>Personal Goals</NavLink>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                    </NavBtn>
+                </Nav>
+            </>
+        )
     }
-    
+
 }
 
 export default Navbar;
--- src/navigation/navcomponents.js
+++ src/navigation/navcomponents.js
@@ -1,79 +1,88 @@
-import {FaBars} from 'react-icons/fa';
-import { NavLink as Link} from 'react-router-dom';
-import styled from 'styled-components'
+import { FaBars } from 'react-icons/fa';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-//Exporting a styled nav component with preset style
+// Exporting a styled nav component with preset style
 export const Nav = styled.nav`
-    background: #1c3144;
-    height: 85px;
-    display: flex;
-    justify-content" space-between;
-    padding: 0.2rem calc((100vw - 1000px) /2);
-    z-index:12;
+  background: #1c3144;
+  height: 85px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center; // Center the content vertically
+  padding: 0.2rem calc((100vw - 1000px) / 2);
+  z-index: 12;
+  
+  
+`;
+
+export const NavContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
 `;
 
 export const NavLink = styled(Link)`
-    color: #808080;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-    &.active{
-        color #000000;
-    }
+  color: #808080;
+  text-decoration: none;
+  padding: 0 1rem;
+  cursor: pointer;
+  &.active {
+    color: #000000;
+  }
 `;
 
-
 export const Bars = styled(FaBars)`
-    color: #808080;
-    display: none;
-    @media screen and (max-width: 768px){
-        display: block;
-        postition: absolute;
-        top: 0;
-        right: 0;
-        transform: translate( -100%, 75%);
-        font-size: 1.8rem;
-        cursor: pointer;
-    }
+  color: #808080;
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
 `;
 
 export const NavMenu = styled.div`
-    display: flex;
-    align-items: center;
-    margin-right: 24px; 
-    @media screen and (max-width: 768px){
-        display: none;
-    }
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+  @media screen and (max-width:768px){
+    display: none;
+  }
 `;
 
 export const NavBtn = styled.nav`
-    display: flex;
-    align-items: center;
-    margin-right: 24px;
-    @media screen and (max-width: 768px){
-        display: none;
-    } 
+  display: flex;
+  align-items: center;
+  margin-right: 24px; 
+  @media screen and (max-width:768px){
+    display: none;
+  }
 `;
 
-//Link inside a nav button (Not a tab)
+// Link inside a nav button (Not a tab)
 export const NavBtnLink = styled(Link)`
-    background: #808080;
-    color: #000000;
-    padding: 10px 20px;
-    outline: none;
-    border: none;
-    cursor: pointer;
+  background: #808080;
+  color: #000000;
+  padding: 10px 20px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  margin-left: 400px;
+  &:hover {
     transition: all 0.2s ease-in-out;
-    text-decoration: none;
-    margin-left: 24px;
-    &:hover{
-        transition: all 0.2s ease-in-out;
-        background: #fff;
-        color: #808080;
-    }
-    border-radius: 4px;
-    
+    background: #fff;
+    color: #808080;
+  }
+  border-radius: 4px;
 `;
+
+
