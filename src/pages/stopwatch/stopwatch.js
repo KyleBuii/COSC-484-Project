82afec6c3a966575e0fb43a9e91e9c24dd03a119
kennyasare <143973599+kennyasare@users.noreply.github.com>
2023-11-28 12:49:59 -0500

    Update stopwatch.js

--- src/pages/stopwatch/stopwatch.js
+++ src/pages/stopwatch/stopwatch.js
@@ -1,89 +1,75 @@
-let stopwatchInterval;
-let stopwatchRunning = false;
-let stopwatchTime = 0;
+import React, { useState, useEffect } from 'react';
 
-let timerInterval;
-let timerRunning = false;
-let timerTime = 0;
-let timerEndTime = 0;
+const StopwatchTimer = () => {
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [timerDuration, setTimerDuration] = useState(60);
+  const [isRunning, setIsRunning] = useState(false);
 
-function startStop() {
-    if (stopwatchRunning) {
-        clearInterval(stopwatchInterval);
-        document.getElementById("startStop").textContent = "Start";
-    } else {
-        stopwatchInterval = setInterval(updateStopwatch, 1000);
-        document.getElementById("startStop").textContent = "Stop";
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setElapsedTime(prevTime => prevTime + 1);
+      }, 1000);
     }
-    stopwatchRunning = !stopwatchRunning;
-}
 
-function reset() {
-    clearInterval(stopwatchInterval);
-    document.getElementById("stopwatchDisplay").textContent = "00:00:00";
-    document.getElementById("startStop").textContent = "Start";
-    stopwatchRunning = false;
-    stopwatchTime = 0;
-}
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning]);
 
-function updateStopwatch() {
-    stopwatchTime++;
-    const hours = Math.floor(stopwatchTime / 3600);
-    const minutes = Math.floor((stopwatchTime % 3600) / 60);
-    const seconds = stopwatchTime % 60;
-    document.getElementById("stopwatchDisplay").textContent = `${hours}:${minutes}:${seconds}`;
-}
+  const startStopwatch = () => {
+    setIsRunning(true);
+  };
 
-function setTimer() {
-    const timerInput = document.getElementById("timerInput");
-    const inputTime = timerInput.value;
-    timerEndTime = parseInt(inputTime) * 60;
-    updateTimerDisplay(timerEndTime);
-}
+  const stopStopwatch = () => {
+    setIsRunning(false);
+  };
 
-function startTimer() {
-    if (!timerRunning && timerEndTime > 0) {
-        timerInterval = setInterval(updateTimer, 1000);
-        timerRunning = true;
-        document.getElementById("timerInput").disabled = true; 
-        document.getElementById("startTimer").textContent = "Pause";
-        document.getElementById("startTimer").onclick = pauseTimer;
-    }
-}
+  const resetStopwatch = () => {
+    setElapsedTime(0);
+    setIsRunning(false);
+  };
 
-function pauseTimer() {
-    if (timerRunning) {
-        clearInterval(timerInterval);
-        timerRunning = false;
-        document.getElementById("startTimer").textContent = "Resume";
-        document.getElementById("startTimer").onclick = resumeTimer;
-    }
-}
+  const startTimer = () => {
+    setIsRunning(true);
+  };
 
-function resumeTimer() {
-    if (!timerRunning) {
-        timerInterval = setInterval(updateTimer, 1000);
-        timerRunning = true;
-        document.getElementById("startTimer").textContent = "Pause";
-        document.getElementById("startTimer").onclick = pauseTimer;
-    }
-}
+  const stopTimer = () => {
+    setIsRunning(false);
+  };
 
-function updateTimer() {
-    if (timerEndTime <= 0) {
-        clearInterval(timerInterval);
-        timerRunning = false;
-        document.getElementById("timerInput").disabled = false; 
-        document.getElementById("startTimer").textContent = "Start Timer";
-        alert("Timer has finished!");
-    } else {
-        timerEndTime--;
-        updateTimerDisplay(timerEndTime);
-    }
-}
+  const resetTimer = () => {
+    setElapsedTime(0);
+    setIsRunning(false);
+  };
 
-function updateTimerDisplay(time) {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    document.getElementById("timerDisplay").textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-}
+  const formatTime = timeInSeconds => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Stopwatch</h2>
+        <p>{formatTime(elapsedTime)}</p>
+        <button onClick={startStopwatch}>Start</button>
+        <button onClick={stopStopwatch}>Stop</button>
+        <button onClick={resetStopwatch}>Reset</button>
+      </div>
+
+      <div>
+        <h2>Timer</h2>
+        <p>{formatTime(timerDuration - elapsedTime)}</p>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default StopwatchTimer;
