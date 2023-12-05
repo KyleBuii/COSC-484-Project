3c0c5abebc379bdc901436abe00f8f46f15922e4
Alex-S0Y <a.soyoh@gmail.com>
2023-12-05 14:44:55 -0500

    found some errors

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,34 +1,37 @@
-import Suggestion from "./Suggestion/suggestion"
-import SignIn from './User/signIn'
-import LogIn from './User/logIn'
-import Personalgoals from './personalgoals/personalgoals'
-import Home from "./home"
-import Calculator from './Calculator/Calculator'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Navbar from '../navigation/navbar'
-import App from './User/App'
+import Suggestion from "./Suggestion/suggestion";
+import SignIn from "./User/signIn";
+import LogIn from "./User/logIn";
+import Personalgoals from "./personalgoals/personalgoals";
+import Home from "./home";
+import Calculator from "./Calculator/Calculator";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "../navigation/navbar";
+import App from "./User/App";
+// import Apps from "./Calendars/Apps";
+import Stopwatch from "./stopwatch/stopwatch";
 
 function Layout() {
-    return (
-        <>
-            <Router>
-            <Navbar/>
-                <Routes>
-                    
-                        <Route path="/" element={<Home></Home>}></Route>
-                        <Route path="/suggestion" element={<Suggestion></Suggestion>}></Route>
-                        <Route path="/personalgoals" element={<Personalgoals></Personalgoals>}></Route>
-                        <Route path="/calculator" element={<Calculator></Calculator>}></Route>
-                        <Route path="/app" element={<App></App>}></Route>
-                        
-                        <Route path="/login" element={<LogIn></LogIn>}></Route>
-                        <Route path="/signin" element={<SignIn></SignIn>}></Route>
-                        
-                        
-                </Routes>
-            </Router>
-        </>
-    )
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/suggestion" element={<Suggestion></Suggestion>}></Route>
+          <Route
+            path="/personalgoals"
+            element={<Personalgoals></Personalgoals>}
+          ></Route>
+          <Route path="/calculator" element={<Calculator></Calculator>}></Route>
+          <Route path="/app" element={<App></App>}></Route>
+          {/* <Route path="/apps" element={<Apps></Apps>}></Route> */}
+          <Route path="/stopwatch" element={<Stopwatch></Stopwatch>}></Route>
+          <Route path="/login" element={<LogIn></LogIn>}></Route>
+          <Route path="/signin" element={<SignIn></SignIn>}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
