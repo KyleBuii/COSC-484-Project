3ad7c01d83648b184557fcf17d3a08969f67371b
BS32 <eguzmandls@gmail.com>
2023-12-05 13:17:46 -0500

    csaew

--- src/navigation/navbar.js
+++ src/navigation/navbar.js
@@ -1,19 +1,19 @@
+
 import { Component } from "react";
-import { GiGymBag } from "react-icons/gi";
+import {GiGymBag} from "react-icons/gi"
 import {
-  Nav,
-  NavBtn,
-  NavBtnLink,
-  NavMenu,
-  Bars,
-  NavLink,
-  NavAuth,
-  WebsiteName,
+    Nav,
+    NavBtn,
+    NavBtnLink,
+    NavMenu,
+    Bars,
+    NavLink,
+    NavAuth,
+    WebsiteName,
 } from "./navcomponents";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-<<<<<<< HEAD
     render() {
         return (
             <>
@@ -26,51 +26,23 @@ class Navbar extends Component {
                         <NavLink to="/calculator" activeStyle>Fitness Calculator</NavLink>
                         <NavLink to="/"><GiGymBag/></NavLink>
                     </NavMenu>
-=======
-  render() {
-    return (
-      <>
-        <Nav>
-          <Bars />
-          <WebsiteName>MyFitnessJournal</WebsiteName>
-          <NavMenu>
-            <NavLink to="/personalgoals" activeStyle>
-              Personal Goals
-            </NavLink>
-            <NavLink to="/suggestion" activeStyle>
-              Suggestion
-            </NavLink>
-            <NavLink to="/calculator" activeStyle>
-              Fitness Calculator
-            </NavLink>
-            <NavLink to="/apps" activeStyle>
-              Calendar
-            </NavLink>
-            <NavLink to="/app" activeStyle>
-              Tests
-            </NavLink>
->>>>>>> 62005ce255fe4ab25066f1bc8a5c499d14573b9a
 
-            {/* Delete me */}
-            {/* <NavLink to="/chicken" activeStyle>chicken</NavLink> */}
-            <NavLink to="/">
-              <GiGymBag />
-            </NavLink>
-          </NavMenu>
+                    <NavAuth>
+                    <NavBtn>
+                        <NavBtnLink to="/login">Log In</NavBtnLink>
+                    </NavBtn>
 
-          <NavAuth>
-            <NavBtn>
-              <NavBtnLink to="/login">Log In</NavBtnLink>
-            </NavBtn>
+                    <NavBtn>
+                        <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                    </NavBtn>
+                    </NavAuth>
+                </Nav>
+            </>
+        )
+    }
 
-            <NavBtn>
-              <NavBtnLink to="/signin">Sign In</NavBtnLink>
-            </NavBtn>
-          </NavAuth>
-        </Nav>
-      </>
-    );
-  }
 }
+
+
 
 export default Navbar;
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -19,10 +19,7 @@ function Layout() {
                         <Route path="/suggestion" element={<Suggestion></Suggestion>}></Route>
                         <Route path="/personalgoals" element={<Personalgoals></Personalgoals>}></Route>
                         <Route path="/calculator" element={<Calculator></Calculator>}></Route>
-                        <Route path="/app" element={<App></App>}></Route>
 
-                        {/* Delete me */}
-                        {/* <Route path="/chicken" element={<UserForm></UserForm>}></Route> */}
                         
                         <Route path="/login" element={<LogIn></LogIn>}></Route>
                         <Route path="/signin" element={<SignIn></SignIn>}></Route>
